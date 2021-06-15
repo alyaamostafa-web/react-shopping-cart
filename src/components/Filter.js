a7d8e2690fd8c3d2d22c8ffb0c35fee061a2293f
alyaamostafa-web <alyaamostafa8@gmail.com>
2021-06-16 01:25:39 +0200

    cart component

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -10,7 +10,7 @@ export default class Filter extends Component {
           <select value={this.props.sort} onChange={this.props.sortProducts}>
             <option>Latest</option>
             <option value="lowest">Lowest</option>
-            <option value="heighest">Heighest</option>
+            <option value="highest">Highest</option>
           </select>
         </div>
         <div className="filter-size">
